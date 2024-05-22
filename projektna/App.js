3f267ec151576871a06cc2411e220489568a29d1
SashK02 <saso.krepek@student.um.si>
2024-05-22 13:04:44 +0200

    Add files via upload

--- projektna/App.js
+++ projektna/App.js
@@ -1,11 +1,19 @@
+//import '@tamagui/core/reset.css' //To ne sme biti, drugače native ne dela (je samo za web)
+import {  TamaguiProvider, Theme } from 'tamagui'
+import config from './tamagui.config'
+import Inicialization from './components/Inicialization/Inicialization';
+import NavigationContainerComponent from './navigation/Navigation';
 import { LogBox } from 'react-native';
 
 
   export default function App() {
   LogBox.ignoreLogs([`fontFamily "Inter`]); //Hide warnings
   return (
-    <div>
-      <h1>Pozdrav</h1>
-    </div>
+    <TamaguiProvider config={config}>
+      <Inicialization />
+      <Theme name="blue">
+        <NavigationContainerComponent />
+      </Theme>
+    </TamaguiProvider>
   );
 }
